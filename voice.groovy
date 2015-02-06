686e2682aba75f1b5d0b3451aea74ddbe1044438
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-02-06 23:04:27 +0100

    update plugin; move voice from build script into custom config file

--- voice.groovy
+++ voice.groovy
@@ -1,5 +1,13 @@
-license {
-    name = 'Creative Commons Attribution-NoDerivs 3.0 Unported'
-    shortName = 'CC-BY-ND'
-    url = 'http://mary.dfki.de/download/by-nd-3.0.html'
+voice {
+    name = 'dfki-ot-hsmm'
+    language = 'tr'
+    gender = 'male'
+    type = 'hsmm'
+    description = 'A male Turkish hidden semi-Markov model voice'
+    license {
+        name = 'Creative Commons Attribution-NoDerivs 3.0 Unported'
+        shortName = 'CC-BY-ND'
+        url = 'http://mary.dfki.de/download/by-nd-3.0.html'
+    }
+    samplingRate = 16000
 }
